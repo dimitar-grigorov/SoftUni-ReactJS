a2622bc5f80663367c2a7a21bf0c3dd4a4cbc316
dimitar-grigorov <mitak.backtrack@gmail.com>
2023-11-23 06:32:50 +0200

    Add register page

--- 06.Routing-Workshop/client/src/App.jsx
+++ 06.Routing-Workshop/client/src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import GameList from "./components/game-list/GameList"
 import GameCreate from "./components/game-create/GameCreate"
+import Login from "./components/login/Login"
+import Register from "./components/register/Register"
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<GameList />} />
         <Route path="/games/create" element={<GameCreate />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   )
